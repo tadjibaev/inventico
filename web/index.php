6de38335b28dce7fcd427cdf831a0efc225f87ac
Tadjibaev Ulugbek <tadjibaev.ulugbek@gmail.com>
2015-03-03 01:37:48 +0500

    sdf

--- web/index.php
+++ web/index.php
@@ -3,7 +3,8 @@
 // comment out the following two lines when deployed to production
 defined('YII_DEBUG') or define('YII_DEBUG', true);
 defined('YII_ENV') or define('YII_ENV', 'dev');
-
+error_reporting(E_ALL);
+ini_set( 'display_errors', 1);
 require(__DIR__ . '/../vendor/autoload.php');
 require(__DIR__ . '/../vendor/yiisoft/yii2/Yii.php');
 
